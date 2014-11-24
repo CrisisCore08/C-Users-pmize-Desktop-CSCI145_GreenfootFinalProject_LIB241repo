4732121be6ad88296cbb6333eb2f8da028ebe6e7
unknown <pmize@BEUSCB2437.ds.sc.edu>
2014-11-24 14:31:33 -0500

    Comments for Braces added

---
+++ EvadeGame/Player.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player extends Actor
+{
+    /**
+     * Constructor for Player - nothing to do.   
+     */
+    public void Player()
+    {
+        
+    }//end method Player()
+    /**
+     * Act - do whatever the Player wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+       
+    }//end method act()    
+}//end class Player
